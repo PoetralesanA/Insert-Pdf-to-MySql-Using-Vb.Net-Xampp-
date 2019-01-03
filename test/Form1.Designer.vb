553b66b0f73e62de91077c317242ab678c3affdb
PoetralesanA <poetralesana13@gmail.com>
2019-01-04 07:13:25 +0800

    -imitializing-

---
+++ test/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.nm_file = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.AxAcroPDF1 = New AxAcroPDFLib.AxAcroPDF()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.silent2 = New System.Windows.Forms.Label()
+        Me.silent = New System.Windows.Forms.Label()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.btn_view = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.AxAcroPDF1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(6, 27)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(147, 53)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(6, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(374, 99)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Function"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(215, 27)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(153, 53)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Upload"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 11)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Path :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(4, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 11)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Status : "
+        '
+        'nm_file
+        '
+        Me.nm_file.AutoSize = True
+        Me.nm_file.Font = New System.Drawing.Font("Segoe UI", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nm_file.Location = New System.Drawing.Point(3, 25)
+        Me.nm_file.Name = "nm_file"
+        Me.nm_file.Size = New System.Drawing.Size(44, 11)
+        Me.nm_file.TabIndex = 5
+        Me.nm_file.Text = "Name File :"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.AxAcroPDF1)
+        Me.GroupBox2.Location = New System.Drawing.Point(3, 163)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(694, 304)
+        Me.GroupBox2.TabIndex = 7
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Viewer"
+        '
+        'AxAcroPDF1
+        '
+        Me.AxAcroPDF1.Enabled = True
+        Me.AxAcroPDF1.Location = New System.Drawing.Point(13, 19)
+        Me.AxAcroPDF1.Name = "AxAcroPDF1"
+        Me.AxAcroPDF1.OcxState = CType(resources.GetObject("AxAcroPDF1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxAcroPDF1.Size = New System.Drawing.Size(668, 268)
+        Me.AxAcroPDF1.TabIndex = 0
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.nm_file)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Font = New System.Drawing.Font("Segoe UI", 6.0!)
+        Me.GroupBox3.Location = New System.Drawing.Point(6, 102)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(374, 59)
+        Me.GroupBox3.TabIndex = 8
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Detail"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(397, 21)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(294, 105)
+        Me.DataGridView1.TabIndex = 11
+        '
+        'silent2
+        '
+        Me.silent2.AutoSize = True
+        Me.silent2.Location = New System.Drawing.Point(102, 63)
+        Me.silent2.Name = "silent2"
+        Me.silent2.Size = New System.Drawing.Size(39, 13)
+        Me.silent2.TabIndex = 6
+        Me.silent2.Text = "Label3"
+        '
+        'silent
+        '
+        Me.silent.AutoSize = True
+        Me.silent.Location = New System.Drawing.Point(100, 33)
+        Me.silent.Name = "silent"
+        Me.silent.Size = New System.Drawing.Size(39, 13)
+        Me.silent.TabIndex = 10
+        Me.silent.Text = "Label3"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.btn_view)
+        Me.GroupBox4.Controls.Add(Me.silent)
+        Me.GroupBox4.Controls.Add(Me.silent2)
+        Me.GroupBox4.Location = New System.Drawing.Point(386, 2)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(311, 159)
+        Me.GroupBox4.TabIndex = 12
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Data"
+        '
+        'btn_view
+        '
+        Me.btn_view.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_view.Location = New System.Drawing.Point(10, 125)
+        Me.btn_view.Name = "btn_view"
+        Me.btn_view.Size = New System.Drawing.Size(296, 28)
+        Me.btn_view.TabIndex = 13
+        Me.btn_view.Text = "View"
+        Me.btn_view.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(704, 479)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.AxAcroPDF1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents nm_file As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents AxAcroPDF1 As AxAcroPDFLib.AxAcroPDF
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents silent2 As System.Windows.Forms.Label
+    Friend WithEvents silent As System.Windows.Forms.Label
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents btn_view As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ test/Form1.vb
@@ -0,0 +1,152 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    '// ----------------- Config Connection -----------------//
+    Dim strG As String = "server=127.0.0.1;" &
+                         "uid=root;" &
+                         "pwd=mastah;" &
+                         "database=testupload"
+    Dim conn As New MySqlConnection(strG)
+    '// ------------------------------------------------------//
+    Dim query, getName, _path, secretDtrows 'variable
+    Dim cmnd As MySqlCommand ' command sql
+    Private Sub ViewDG()
+        Dim dt_table As New DataTable
+        Dim dtAdptr As New MySqlDataAdapter
+        Dim bndSource As New BindingSource
+        With dtAdptr
+            Try
+                conn.Open()
+                query = "select _namefile as 'Name File', location as 'Path Dircetory' from pdfupload"
+
+                cmnd = New MySqlCommand(
+                                        query, conn
+                                        )
+                .SelectCommand = cmnd
+                .Fill(dt_table)
+                bndSource.DataSource = dt_table
+
+                DataGridView1.DataSource = dt_table
+
+            Catch ex As Exception
+                MsgBox("Check Your Connection!!",
+                                MsgBoxStyle.Exclamation, "Lost Conn")
+            End Try
+            conn.Close()
+        End With
+    End Sub
+    Private Sub fm_Load() Handles MyBase.Load
+        silent.Visible = False
+        silent2.Visible = False
+        ViewDG()
+    End Sub
+    Private Sub BrowseFile() Handles Button2.Click
+        _ResetText()
+        Button1.Enabled = True
+        OpenFileDialog1.Filter = " Chose Fucking File (*.pdf) | *.pdf;"
+        OpenFileDialog1.FileName = ""
+        OpenFileDialog1.Title = "Select File Pdf.."
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub OpfD() Handles OpenFileDialog1.FileOk
+        _path = System.IO.Path.
+                GetFullPath(
+                            OpenFileDialog1.FileName
+                            )
+        getName = System.IO.Path.
+            GetFileName(OpenFileDialog1.FileName
+                        )
+
+
+        Label1.Text = "Path : " & _path
+        nm_file.Text = "Name File : " & getName
+
+        AxAcroPDF1.src = _path
+
+
+        silent2.Text = _path
+        silent.Text = silent2.Text.Replace("\", "\\")
+    End Sub
+    Private Sub sendtodb() Handles Button1.Click
+        If Label1.Text = "Path :" Then
+            MsgBox("Upload ditolak!" & vbNewLine &
+                   "Anda Belum memilih file", MsgBoxStyle.Exclamation, "")
+        Else
+
+            With Label2
+                Try
+                    conn.Open()
+                    query = "insert into pdfupload (_namefile, location) values ('" & getName & "', '" & silent.Text & "')"
+
+                    cmnd = New MySqlCommand(query,
+                                             conn)
+
+                    Dim _read As MySqlDataReader
+                    _read = cmnd.ExecuteReader
+                    .Text = "Status : Sucesfully"
+                    If .Text = "Status : Sucesfully" Then
+                        .ForeColor = Color.Green
+                        Button1.Enabled = False
+                    End If
+                Catch ex As Exception
+                    .Text = "Status : Error | Check Your Conection!!"
+                    .ForeColor = Color.Red
+                End Try
+                conn.Close()
+                ViewDG() 'refresh Datagrid
+                x()
+            End With
+        End If
+    End Sub
+
+    Private Sub _ResetText()
+        Label1.Text = "Path : "
+        nm_file.Text = "Name File : "
+        Label2.Text = "Status : "
+        Label2.ForeColor = Color.Black
+
+        silent.Text = ""
+        silent2.Text = ""
+    End Sub
+#Region "jaja"
+    Private Sub x()
+        If Not Me.Text = "Form1" Then
+            MsgBox(Me.Text & "???" & vbNewLine &
+                   "Hiks :(", MsgBoxStyle.Critical, "")
+        End If
+    End Sub
+#End Region
+    Private Sub Dataiew(sender As Object, e As EventArgs) Handles btn_view.Click
+        If Label1.Text = "Path :" Then
+            MsgBox("Anda Belum memilih data Pada Datagrid", MsgBoxStyle.
+                                                         Exclamation, "Access Danied!!")
+        Else
+            '// Lakukan Prosses
+
+            AxAcroPDF1.src = secretDtrows
+        End If
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        Try
+            Dim indx As Integer
+            Dim _selectrow As DataGridViewRow
+            indx = e.RowIndex
+
+            _selectrow = DataGridView1.
+                         Rows(indx)
+
+            Label1.Text = "Path : " & _selectrow.Cells(0).
+                                      Value.ToString()
+            nm_file.Text = "Name File : " & _selectrow.Cells(1).
+                                      Value.ToString()
+
+
+
+            secretDtrows = _selectrow.Cells(1).
+                                      Value.ToString() 'get data click DG
+        Catch ex As Exception
+            'errorbypass
+        End Try
+    End Sub
+End Class
